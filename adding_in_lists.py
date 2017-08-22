c9dec7c02c92db3fefdb76ce6717b1b80c1c2fcd
irimina <irimina@gmail.com>
2017-08-22 15:40:27 -0400

    Create adding_in_lists.py

---
+++ adding_in_lists.py
@@ -0,0 +1,10 @@
+# You can access an individual item on the list by its index. 
+# Write a statement that prints the result of adding the second and fourth items of the list.
+# Make sure to access the list by index!
+
+numbers = [5, 6, 7, 8]
+
+print("Adding the numbers at indices 0 and 2...")
+print(numbers[0] + numbers[2])
+print("Adding the numbers at indices 1 and 3...")
+print(numbers[1]+numbers[3])
