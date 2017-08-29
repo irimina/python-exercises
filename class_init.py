348ad5c83ae8e33a41fdbc8ee7231265004699e8
irimina <irimina@gmail.com>
2017-08-29 09:35:06 -0400

    Create class_init.py

---
+++ class_init.py
@@ -0,0 +1,15 @@
+# use a class with init 
+
+
+class Enemy:
+    def __init__(self,x):
+        self.energy = x
+
+    def get_energy(self):
+        print(self.energy)
+
+jason = Enemy(5)
+sandy = Enemy(19)
+
+jason.get_energy()
+sandy.get_energy()
