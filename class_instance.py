ce36ebefbe9023b3f29fafdc28dad23920a8348f
irimina <irimina@gmail.com>
2017-08-29 09:19:17 -0400

    Create class_instance.py

---
+++ class_instance.py
@@ -0,0 +1,17 @@
+class Girl:
+
+    # this common to all objects in the class, class variable
+    gender = 'female'
+
+    # this is unique to the object or that specific instance of the class; this is the instance variable
+    def __init__(self, name):
+        self.name = name
+
+first_girl= Girl('Rachel')
+another_girl = Girl('Sandy')
+
+print(first_girl.gender)
+print(another_girl.gender)
+
+print(first_girl.name)
+print(another_girl.name)
