ffd24923c7b15d23f8c38ebbeb48baad4bf00b4c
irimina <irimina@gmail.com>
2017-08-29 09:18:04 -0400

    Create addByIndex.py

---
+++ addByIndex.py
@@ -0,0 +1,9 @@
+
+# adding numbers by indexes in a list. Can you average and do other operations? 
+
+numbers = [5, 6, 7, 8]
+
+print("Adding the numbers at indices 0 and 2...")
+print(numbers[0] + numbers[2])
+print("Adding the numbers at indices 1 and 3...")
+print(numbers[1]+numbers[3])
