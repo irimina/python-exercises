775959242c15a9140a0b6c603059cc0f34ca6c4b
irimina <irimina@gmail.com>
2018-08-05 12:07:12 -0400

    Update nestedLoop.py

--- nestedLoop.py
+++ nestedLoop.py
@@ -6,4 +6,26 @@ for i in range(0,5):
           print("*", end="") # print the lines of stars across
      print() # go to a new line
      
-     
+'''
+The order of execution for the nested loop is:
+
+outer loop initialize variable
+
+outer loop test condition
+
+inner loop initialize variable
+
+inner loop test condition
+
+body of inner loop
+
+inner loop update variable
+
+repeat inner loop
+
+outer loop update variable
+
+repeat outer loop
+
+
+'''
